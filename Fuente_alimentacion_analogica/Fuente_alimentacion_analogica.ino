609ba5eec565304f58bdd98201a753231843d527
renatolrr <renatolrr@gmail.com>
2015-08-09 12:04:05 +0200

    Fuente alimentación de seguridad con Arduino

---
+++ Fuente_alimentacion_analogica/Fuente_alimentacion_analogica.ino
@@ -0,0 +1,53 @@
+
+//Fuente de seguridad analogica
+//Renato Ramirez
+
+#include <EEPROM.h>
+
+int button1 = 10; // Boton de envio
+int button2 = 12; // Boton de reset
+int relayPin = 13;// Para activar transistor
+int pot = 5; // Potenciometro de pass
+
+
+void setup()
+{
+  pinMode(button1, INPUT); // envio como input
+  pinMode(button2, INPUT); // reset como input
+  pinMode(relayPin, OUTPUT);  // set pin as an output
+  pinMode(pot, INPUT); // pass como input
+  Serial.begin(9600); //serial 
+}
+
+void loop()
+{
+  // reset
+  int breset;
+  breset = digitalRead(button2);
+  if(breset == LOW){
+    int ini=0;
+    EEPROM.write(1,ini);
+  }
+  // lectura pass
+  int pass;
+  pass = analogRead(pot);
+  // valor del pass
+  int valor;
+  valor = EEPROM.read(1);
+  //cambio de valor
+  if (Serial.available() > 0) {   
+      valor=Serial.read();
+      EEPROM.write(1,valor); 
+      }
+  // comprobar pass
+  if(button1 == LOW){
+    if((pass==valor)||(valor==0)){
+      digitalWrite(relayPin,HIGH);
+     }
+  }
+} 
+  
+
+
+
+
---
+++ Fuente_alimentacion_android/Fuente_alimentacion_android.ino
@@ -0,0 +1,24 @@
+//Fuente alimentacion android
+
+boolean estado;
+ 
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  randomSeed(analogRead(0));
+  estado = false;
+}
+ 
+void loop()
+{
+    delay(1000);
+    Serial.write(random(40));
+    while(Serial.available() > 0)
+    {
+      Serial.read();
+      estado = !estado;
+      digitalWrite(13, estado);
+    }
+}
